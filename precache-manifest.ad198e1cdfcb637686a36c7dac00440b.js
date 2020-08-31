88dcefe6c774ca813e1fd3e76fe6bcacdf501ce1
mostafa ghazaly <ghazalymostafa07@gmail.com>
2020-08-31 05:25:39 +0200

    Updates

---
+++ precache-manifest.ad198e1cdfcb637686a36c7dac00440b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7b2e392b2f5ac784ba9dfd84040d70",
+    "url": "/covid19-app/index.html"
+  },
+  {
+    "revision": "8f77f9ba28c36a0f5c5d",
+    "url": "/covid19-app/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "780d7ea8520b7da2f80d",
+    "url": "/covid19-app/static/css/main.4fefb029.chunk.css"
+  },
+  {
+    "revision": "8f77f9ba28c36a0f5c5d",
+    "url": "/covid19-app/static/js/2.fefa809c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-app/static/js/2.fefa809c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "780d7ea8520b7da2f80d",
+    "url": "/covid19-app/static/js/main.618f82a4.chunk.js"
+  },
+  {
+    "revision": "e1594e6080d02c5d5197",
+    "url": "/covid19-app/static/js/runtime-main.b43b76b1.js"
+  }
+]);
